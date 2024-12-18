5d9662ed93bb47ca0fb2a68c4aa3d6b00e727efe
CHAIMA <seghirchaimae@gmail.com>
2024-12-18 20:31:32 +0100

    Initial Commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RighesJoy.Models;
+using System.Collections.Generic;
+
+namespace RighesJoy.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Static list to simulate a DB 
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Arbu_Clear", Description = "Alpha Arbutine", Price = 199 },
+            new Product { Id = 2, Name = "Glyco_Peel", Description = "Glycolic Acide", Price = 299 },
+            new Product { Id = 3, Name = "Glow_C", Description = "Vitamine C", Price = 100 },
+            new Product { Id = 4, Name = "Block_Sun", Description = "Sun Screen", Price = 250 },
+            new Product { Id = 5, Name = "Mice_Clean", Description = "Micellar water", Price = 50}
+        };
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public IActionResult Catalog()
+        {
+            return View(products); // Gives list of products to the view Catalog.cshtml
+        }
+
+        // Action To show product's details 
+        public IActionResult Details(int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View("ProductDescription",product); // Use the view ProductDescription.cshtml
+        }
+    }
+}
+
---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace RighesJoy.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } // Unique Identifier
+        public string Name { get; set; } // Name Of Product
+        public string Description { get; set; } // Description Of Product
+        public decimal Price { get; set; } // Price Of Product
+    }
+}
+
+
+
+
